5a42f5f677aa423fb99364a7b174d0c033810e9f
@revoltcode <oddybassey@gmail.com>
2021-01-19 17:01:39 +0100

    Added web data model. closes #2

---
+++ src/main/java/com/revoltcode/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,28 @@
+package com.revoltcode.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModified;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private long upc;
+    private BigDecimal price;
+    private Integer quantity;
+}
---
+++ src/main/java/com/revoltcode/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.revoltcode.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALA_ALE, SAISON;
+}
