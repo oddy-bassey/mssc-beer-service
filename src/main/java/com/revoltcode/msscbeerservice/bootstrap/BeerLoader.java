881363adf42d1ff10f9ac06d01b1d1a144f34618
@revoltcode <oddybassey@gmail.com>
2021-02-10 16:41:30 +0100

    Implemented bean validation on the BerrDTO. closes #8

--- src/main/java/com/revoltcode/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/revoltcode/msscbeerservice/bootstrap/BeerLoader.java
@@ -11,9 +11,9 @@ import java.math.BigDecimal;
 @Component
 public class BeerLoader implements CommandLineRunner {
 
-    @Autowired
     private final BeerRepository beerRepository;
 
+    @Autowired
     public BeerLoader(BeerRepository beerRepository) {
         this.beerRepository = beerRepository;
     }
@@ -28,17 +28,19 @@ public class BeerLoader implements CommandLineRunner {
         if(beerRepository.count() == 0){
 
             beerRepository.save(Beer.builder()
-            .beerName("Stout")
-            .beerStyle("IPA")
-            .quantityToBrew(200)
-            .upc(337010000001L)
-            .price(new BigDecimal(12.95))
-            .build());
+                .beerName("Stout")
+                .beerStyle("IPA")
+                .quantityToBrew(200)
+                .minOnHand(12)
+                .upc(337010000001L)
+                .price(new BigDecimal(12.95))
+                .build());
 
             beerRepository.save(Beer.builder()
                 .beerName("Galaxy cat")
                 .beerStyle("PALE_ALE")
                 .quantityToBrew(200)
+                .minOnHand(12)
                 .upc(337020000001L)
                 .price(new BigDecimal(11.95))
                 .build());
