db13375c6f9619f5e0d0b01c5c18df77d7deebf4
@revoltcode <oddybassey@gmail.com>
2021-03-28 23:08:22 +0100

    Beer Service Parent POM Configuration. closes #22

--- target/generated-sources/annotations/com/revoltcode/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/revoltcode/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-12T15:46:14+0100",
+    date = "2021-03-28T20:20:01+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.7-ea (Debian)"
 )
 @Component
--- target/generated-sources/annotations/com/revoltcode/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/com/revoltcode/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -1,26 +1,19 @@
 package com.revoltcode.msscbeerservice.web.mappers;
 
 import com.revoltcode.msscbeerservice.domain.Beer;
-import com.revoltcode.msscbeerservice.domain.Beer.BeerBuilder;
 import com.revoltcode.msscbeerservice.web.model.BeerDto;
-import com.revoltcode.msscbeerservice.web.model.BeerDto.BeerDtoBuilder;
-import com.revoltcode.msscbeerservice.web.model.BeerStyleEnum;
 import javax.annotation.processing.Generated;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-12T15:46:12+0100",
+    date = "2021-03-28T20:20:00+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.7-ea (Debian)"
 )
 @Component
 @Qualifier("delegate")
 public class BeerMapperImpl_ implements BeerMapper {
-
-    @Autowired
-    private DateMapper dateMapper;
 
     @Override
     public BeerDto beerToBeerDto(Beer beer) {
@@ -28,19 +21,9 @@ public class BeerMapperImpl_ implements BeerMapper {
             return null;
         }
 
-        BeerDtoBuilder beerDto = BeerDto.builder();
+        BeerDto beerDto = new BeerDto();
 
-        beerDto.id( beer.getId() );
-        beerDto.version( (int) beer.getVersion() );
-        beerDto.createdDate( dateMapper.asOffsetDateTime( beer.getCreatedDate() ) );
-        beerDto.beerName( beer.getBeerName() );
-        if ( beer.getBeerStyle() != null ) {
-            beerDto.beerStyle( Enum.valueOf( BeerStyleEnum.class, beer.getBeerStyle() ) );
-        }
-        beerDto.upc( beer.getUpc() );
-        beerDto.price( beer.getPrice() );
-
-        return beerDto.build();
+        return beerDto;
     }
 
     @Override
@@ -49,19 +32,9 @@ public class BeerMapperImpl_ implements BeerMapper {
             return null;
         }
 
-        BeerDtoBuilder beerDto = BeerDto.builder();
+        BeerDto beerDto = new BeerDto();
 
-        beerDto.id( beer.getId() );
-        beerDto.version( (int) beer.getVersion() );
-        beerDto.createdDate( dateMapper.asOffsetDateTime( beer.getCreatedDate() ) );
-        beerDto.beerName( beer.getBeerName() );
-        if ( beer.getBeerStyle() != null ) {
-            beerDto.beerStyle( Enum.valueOf( BeerStyleEnum.class, beer.getBeerStyle() ) );
-        }
-        beerDto.upc( beer.getUpc() );
-        beerDto.price( beer.getPrice() );
-
-        return beerDto.build();
+        return beerDto;
     }
 
     @Override
@@ -70,20 +43,8 @@ public class BeerMapperImpl_ implements BeerMapper {
             return null;
         }
 
-        BeerBuilder beer1 = Beer.builder();
+        Beer beer1 = new Beer();
 
-        beer1.id( beer.getId() );
-        if ( beer.getVersion() != null ) {
-            beer1.version( beer.getVersion() );
-        }
-        beer1.createdDate( dateMapper.asTimestamp( beer.getCreatedDate() ) );
-        beer1.beerName( beer.getBeerName() );
-        if ( beer.getBeerStyle() != null ) {
-            beer1.beerStyle( beer.getBeerStyle().name() );
-        }
-        beer1.upc( beer.getUpc() );
-        beer1.price( beer.getPrice() );
-
-        return beer1.build();
+        return beer1;
     }
 }
