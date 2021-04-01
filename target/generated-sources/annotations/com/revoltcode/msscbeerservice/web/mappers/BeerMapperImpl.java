1983baea7448a6405a7be675a52b1efe64b8c903
@revoltcode <oddybassey@gmail.com>
2021-04-01 21:13:36 +0100

    MySQL beer service configurtion. closes #24

---
+++ target/generated-sources/annotations/com/revoltcode/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package com.revoltcode.msscbeerservice.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-04-01T19:36:06+0100",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.7-ea (Debian)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
