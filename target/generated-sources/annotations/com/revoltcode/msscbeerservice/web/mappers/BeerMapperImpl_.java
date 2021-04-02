b83f22d6aab2d6402bd5681700b09e6bf48e3cee
@revoltcode <oddybassey@gmail.com>
2021-04-02 06:43:06 +0100

    fixing hibernate errors on startup with MySQL. closes #27

--- target/generated-sources/annotations/com/revoltcode/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/com/revoltcode/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-04-02T06:01:58+0100",
+    date = "2021-04-02T06:19:26+0100",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.7-ea (Debian)"
 )
 @Component
