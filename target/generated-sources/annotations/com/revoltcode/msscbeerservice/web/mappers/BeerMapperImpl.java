d279edeb7c2b010737fee4bf7f8d7623e5d03c39
@revoltcode <oddybassey@gmail.com>
2021-04-07 21:51:34 +0100

    assignment - DB initialization. closes #28

--- target/generated-sources/annotations/com/revoltcode/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/revoltcode/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-04-02T06:19:27+0100",
+    date = "2021-04-07T21:45:30+0100",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.7-ea (Debian)"
 )
 @Component
